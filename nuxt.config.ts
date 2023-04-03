ea587e0277f014c820392e46011eb0e99eb00440
SEUNGMIN HAN <seungmin_han@kakao.com>
2023-04-03 11:46:49 +0900

    Update: Flicking 라이브러리 글로벌 CSS 추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    css: [
+        "@egjs/vue3-flicking/dist/flicking.css",
+        "@egjs/flicking-plugins/dist/flicking-plugins.css",
+    ]
 })
