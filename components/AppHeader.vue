0db1bb2e238599cd4b004da915172f21d09c76c9
SEUNGMIN HAN <seungmin_han@kakao.com>
2023-04-04 14:51:03 +0900

    Update: photo 메뉴 추가

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,53 +1,50 @@
 <template>
-    <header>
-        <nav class="container">
-        	<ul>
-            	<li><NuxtLink to="/">Home</NuxtLink></li>
-            	<li><NuxtLink to="/color">Colors</NuxtLink></li>
-        	</ul>
-  	    </nav>
-    </header>
+	<header>
+		<nav class="container">
+			<ul>
+				<li><NuxtLink to="/">Home</NuxtLink></li>
+				<li><NuxtLink to="/color">Colors</NuxtLink></li>
+				<li><NuxtLink to="/photo">Photos</NuxtLink></li>
+			</ul>
+		</nav>
+	</header>
 </template>
 
 <script>
-export default {
-	
-}
+	export default {};
 </script>
 
 <style lang="scss">
-    * {
-        margin: 0 auto;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-        text-decoration: none;
-    }
+	* {
+		margin: 0 auto;
+		padding: 0;
+		box-sizing: border-box;
+		list-style: none;
+		text-decoration: none;
+	}
 
-    .container {
-        max-width: 1200px;
-    }
+	.container {
+		max-width: 1200px;
+	}
 </style>
 
 <style lang="scss" scoped>
-
-    header {
-        position: sticky;
-        top: 0;
-        z-index: 1000;
-        padding: 20px;
-        background-color: #333;
-        nav {
-            > ul {
-                > li {
-                    display: inline-block;
-                    padding: 0 10px;
-                    a {
-                        color: #fff;
-                    }
-                }
-            }
-        }
-    }
-    
+	header {
+		position: sticky;
+		top: 0;
+		z-index: 1000;
+		padding: 20px;
+		background-color: #333;
+		nav {
+			> ul {
+				> li {
+					display: inline-block;
+					padding: 0 10px;
+					a {
+						color: #fff;
+					}
+				}
+			}
+		}
+	}
 </style>
