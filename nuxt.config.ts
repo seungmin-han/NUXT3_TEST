da3b686ae61c9d1bc7937e0356d7554c7614ec1f
SEUNGMIN HAN <seungmin_han@kakao.com>
2023-04-04 17:07:23 +0900

    Update: view3d CSS 추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
     css: [
         "@egjs/vue3-flicking/dist/flicking.css",
         "@egjs/flicking-plugins/dist/flicking-plugins.css",
-        "billboard.js/dist/theme/graph.css"
+        "billboard.js/dist/theme/graph.css",
+        "@egjs/vue3-view3d/css/view3d-bundle.css"
     ]
 })
