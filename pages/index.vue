aacfda15b70de849a8590ab5df4a907fa6c69c79
SEUNGMIN HAN <seungmin_han@kakao.com>
2023-04-06 11:30:46 +0900

    Update: @coddicat/vue3-pinch-scroll-zoom 제거

--- pages/index.vue
+++ pages/index.vue
@@ -9,35 +9,11 @@
 				width="1200"
 			/>
 		</ImageView>
-		<ImageZoom
-			class="zoom"
-			:width="1200"
-			:height="600"
-			:origin-x="0"
-			:origin-y="0"
-			:minScale="0.9"
-			:contentWidth="1200"
-			:contentHeight="600"
-		>
-			<img
-				src="~/assets/sky.jpg"
-				alt=""
-				width="1200"
-			/>
-		</ImageZoom>
 	</div>
 </template>
 
 <script setup>
 	import gsap from 'gsap';
-	import ImageZoom from '@coddicat/vue3-pinch-scroll-zoom';
-
-	const zoom = event => {
-		if (isOver) {
-			return;
-		}
-		console.log(isOver, event);
-	};
 
 	onMounted(() => {
 		gsap.fromTo(
@@ -62,9 +38,5 @@
 		justify-content: space-between;
 		align-content: flex-start;
 		flex-wrap: wrap;
-	}
-
-	.zoom {
-		overflow: hidden;
 	}
 </style>
