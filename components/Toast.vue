1fdd07dfbb0012fb2b4c17e3eff8589139704c1e
SEUNGMIN HAN <seungmin_han@kakao.com>
2023-04-06 11:31:30 +0900

    Update: 프리티어 적용 및 지속시간 프롭스 설정 가능 하도록 수정

--- components/Toast.vue
+++ components/Toast.vue
@@ -1,62 +1,80 @@
 <template>
-    <div class="toast_wrap">
-        {{ props.text }} <span v-if="props.color" :style="`background-color:${props.color.backgroundColor}; color:${props.color.fontColor}`">{{ props.color.backgroundColor }}</span>
-    </div>
+	<div class="toast_wrap">
+		{{ props.text }}
+		<span
+			v-if="props.color"
+			:style="`background-color:${props.color.backgroundColor}; color:${props.color.fontColor}`"
+			>{{ props.color.backgroundColor }}</span
+		>
+	</div>
 </template>
 
 <script setup>
-    import gsap from 'gsap';
-    const props = defineProps(['text', 'color']);
-    const emit = defineEmits(['close']);
-    onMounted(() => {
-        gsap.fromTo(
-            ".toast_wrap",
-            {
-                opacity: 0,
-                y: "50%",
-            },
-            {
-                duration: 0.5,
-                opacity: 1,
-                y: 0,
-            }
-        ); 
-        setTimeout(() => {
-            gsap.fromTo(
-                ".toast_wrap",
-                {
-                    opacity: 1,
-                    y: 0,
-                },
-                {
-                    duration: 0.5,
-                    opacity: 0,
-                    y: "50%",
-                }
-            );
-        }, 2500);
-        setTimeout(() => {
-            emit('close');
-        }, 3000);    
-    })
+	import gsap from 'gsap';
+	const props = defineProps({
+		text: {
+			type: String,
+		},
+		color: {
+			type: String,
+		},
+		duration: {
+			type: Number,
+			default: 3000,
+		},
+	});
+	const emit = defineEmits(['close']);
+	onMounted(() => {
+		gsap.fromTo(
+			'.toast_wrap',
+			{
+				opacity: 0,
+				y: '50%',
+			},
+			{
+				duration: 0.5,
+				opacity: 1,
+				y: 0,
+			}
+		);
+		if (props.duration) {
+			setTimeout(() => {
+				gsap.fromTo(
+					'.toast_wrap',
+					{
+						opacity: 1,
+						y: 0,
+					},
+					{
+						duration: 0.5,
+						opacity: 0,
+						y: '50%',
+					}
+				);
+			}, props.duration - 500);
+			setTimeout(() => {
+				emit('close');
+			}, props.duration);
+		}
+	});
 </script>
 
 <style lang="scss" scoped>
-    .toast_wrap {
-        z-index: 300;
-        background-color: rgba($color: #333, $alpha: 0.6);
-        color: #fff;
-        font-weight: bold;
-        line-height: 30px;
-        padding: 10px;
-        position: fixed;
-        bottom: 50px;
-        left: 50%;
-        transform: translateX(-50%);
+	.toast_wrap {
+		z-index: 300;
+		background-color: rgba($color: #333, $alpha: 0.6);
+		color: #fff;
+		font-weight: bold;
+		line-height: 30px;
+		padding: 10px;
+		position: fixed;
+		bottom: 50px;
+		left: 50%;
+		transform: translateX(-50%);
 
-        span {
-            display: inline-block;
-            padding: 5px;
-        }
-    }
+		span {
+			display: inline-block;
+			padding: 5px;
+		}
+	}
 </style>
