af06d68f12e23ed7dd70f5c6dfa688489082288b
SEUNGMIN HAN <seungmin_han@kakao.com>
2023-04-06 13:32:56 +0900

    Update: ssr False

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,5 +5,6 @@ export default defineNuxtConfig({
         "@egjs/flicking-plugins/dist/flicking-plugins.css",
         "billboard.js/dist/theme/graph.css",
         "@egjs/vue3-view3d/css/view3d-bundle.css"
-    ]
+    ],
+    ssr: false,
 })
