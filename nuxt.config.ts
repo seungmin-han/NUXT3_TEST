50aff6249d47fcf2caf0c45a2be2320cb2e2c08c
SEUNGMIN HAN <seungmin_han@kakao.com>
2023-07-07 08:46:59 +0900

    Feat: msw start config/plugin 세팅(동작 확인 중)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    ssr:false,
     css: [
         "@egjs/vue3-flicking/dist/flicking.css",
         "@egjs/flicking-plugins/dist/flicking-plugins.css",
         "billboard.js/dist/theme/graph.css",
         "@egjs/vue3-view3d/css/view3d-bundle.css"
     ],
+    vite: {
+        define: {
+          "process.env.DEBUG": true,
+        },
+    },
+    
 })
---
+++ plugins/mock.js
@@ -0,0 +1,17 @@
+// import { worker } from "@/mocks/browser";
+// import { handlers } from '../mocks/handlers';
+// import { setupWorker } from 'msw';
+
+// 2. Describe network behavior with request handlers.
+
+
+// 3. Start request interception by starting the Service Worker.
+
+
+export default defineNuxtPlugin(() => {
+    // worker.start();
+    // console.log(handlers);
+    // const worker = setupWorker(...handlers);
+    // worker.start({ onUnhandledRequest: "bypass", });
+});
+
