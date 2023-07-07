6c4474dd2af918622fc0738cb4672084d2605d8a
SEUNGMIN HAN <seungmin_han@kakao.com>
2023-07-07 14:43:44 +0900

    Feat: 이미지 클릭 시 모달로 볼 수 있도록 수정

--- components/PhotoGrid.vue
+++ components/PhotoGrid.vue
@@ -1,4 +1,10 @@
 <template>
+	<ImageModal
+		v-for="image in store.modals"
+		:key="image.fileName"
+		:image="image"
+	>
+	</ImageModal>
 	<masonry-infinite-grid
 		class="container"
 		:gap="5"
@@ -11,13 +17,14 @@
 			:key="item.key"
 			:data-grid-groupkey="item.groupKey"
 		>
-			<div class="thumnail">
-				<ImageView>
-					<img
-						:src="item.url"
-						alt=""
-					/>
-				</ImageView>
+			<div
+				class="thumnail"
+				@click="openModal(item)"
+			>
+				<img
+					:src="item.url"
+					alt=""
+				/>
 			</div>
 		</div>
 	</masonry-infinite-grid>
@@ -25,6 +32,10 @@
 
 <script setup>
 	import { MasonryInfiniteGrid } from '@egjs/vue3-infinitegrid';
+	import { useModalStore } from '@/stores/modal';
+
+	const store = useModalStore();
+
 	const imageGroupName = [
 		'KakaoTalk_20230404_102754981',
 		'KakaoTalk_20230404_102818730',
@@ -32,26 +43,35 @@
 		'KakaoTalk_20230404_102858782',
 		'KakaoTalk_20230404_102925099',
 	];
+
 	const getItems = (nextGroupKey, count) => {
 		const nextItems = [];
+
 		for (let i = 0; i < count; ++i) {
 			const nextKey = nextGroupKey * count + i;
-			const url = new URL(
-				`../assets/chungjoo/${imageGroupName[nextGroupKey % 6]}_${
-					('' + nextKey).length === 1 ? '0' + (nextKey % 29) : nextKey % 29
-				}.jpg`,
-				import.meta.url
-			);
-			nextItems.push({ url, groupKey: nextGroupKey, key: nextKey });
+
+			const fileName = `${imageGroupName[nextGroupKey % 6]}_${
+				('' + nextKey).length === 1 ? ('' + (nextKey % 29)).padStart(2, '0') : nextKey % 29
+			}`;
+
+			const url = new URL(`../assets/chungjoo/${fileName}.jpg`, import.meta.url);
+
+			nextItems.push({ url, groupKey: nextGroupKey, key: nextKey, fileName: fileName });
 		}
+
 		return nextItems;
 	};
+
 	let items = ref([...getItems(0, 10)]);
 
 	const onRequestAppend = e => {
 		const nextGroupKey = (+e.groupKey || 0) + 1;
 
 		items.value = [...items.value, ...getItems(nextGroupKey, 10)];
+	};
+
+	const openModal = item => {
+		store.openModal(item);
 	};
 </script>
 
