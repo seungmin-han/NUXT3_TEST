3ac010a45eca064a1bf71c74c70d74e3232d5871
SEUNGMIN HAN <seungmin_han@kakao.com>
2023-07-10 11:37:23 +0900

    Update: color 목록 가져오는 로직 msw로 이동 및 스크롤 스켈레톤자연스럽게수정

--- mocks/handlers.js
+++ mocks/handlers.js
@@ -1,7 +1,46 @@
 // src/mocks/handlers.js
 import { rest } from 'msw'
 
+const colorGenerator = () => {
+  const ARR = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+  let hexCode = '#';
+  for (let i = 0; i < 6; i++) {
+    let rand = Math.round(Math.random() * 15);
+    hexCode += ARR[rand];
+  }
+  return hexCode;
+};
+
+const setFontColor = backgroundColor => {
+  backgroundColor = backgroundColor.slice(1);
+  let r = parseInt(backgroundColor.slice(0, 2), 16);
+  let g = parseInt(backgroundColor.slice(2, 4), 16);
+  let b = parseInt(backgroundColor.slice(4, 6), 16);
+  return r * 0.299 + g * 0.587 + b * 0.114 > 186 ? 'black' : 'white';
+};
+
+const getColors = () => {
+  let colorObj = [];
+  for (let i = 0; i < 40; i++) {
+    let backgroundColor = colorGenerator();
+    colorObj.push({
+      backgroundColor,
+      fontColor: setFontColor(backgroundColor),
+    });
+  }
+  return colorObj;
+};
+
+
 export const handlers = [
+  rest.get('/color', (_, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json(getColors()),
+      ctx.delay(Math.round(Math.random() * 3000 + 500))
+    )
+  }),
+
   rest.post('/login', (req, res, ctx) => {
     // Persist user's authentication in the session
     sessionStorage.setItem('is-authenticated', 'true')
--- pages/color/index.vue
+++ pages/color/index.vue
@@ -1,114 +1,86 @@
 <template>
 	<div class="container">
-  		<h1>Colors</h1>
-  		<div class="wrapper">
-			<div 
+		<h1>Colors</h1>
+		<div class="wrapper">
+			<div
 				@mouseover="setScale($event, true)"
 				@mouseleave="setScale($event, false)"
 				@click="copyColor(color)"
-				v-for="(color, index) in colorList" 
-				:key="color+index" 
-				:style="`background-color: ${color.backgroundColor};`">
-				<span :style="`color: ${color.fontColor};`">{{color.backgroundColor}}</span>
-			</div>		
+				v-for="(color, index) in colorList"
+				:key="color + index"
+				:style="`background-color: ${color.backgroundColor};`"
+			>
+				<span :style="`color: ${color.fontColor};`">{{ color.backgroundColor }}</span>
+			</div>
 			<template v-if="!isLoaded">
-				<div v-for="i in 40" :key="i" class="skeleton">
+				<div
+					v-for="i in skeletonCnt"
+					:key="i"
+					class="skeleton"
+				>
 					<span>{{ i }}</span>
 				</div>
 			</template>
-  		</div>
+		</div>
 	</div>
-	<Toast v-if="showToast" :text="toastText" :color="copiedColor" @close="showToast = false"></Toast>
+	<Toast
+		v-if="showToast"
+		:text="toastText"
+		:color="copiedColor"
+		@close="showToast = false"
+	></Toast>
 </template>
 
 <script setup>
-	import { useInfiniteScroll } from '@vueuse/core'
+	import { useInfiniteScroll } from '@vueuse/core';
+	import axios from 'axios';
 	const showToast = ref(false);
 	const toastText = ref('');
 	const copiedColor = ref({});
 	const el = ref(document);
 
-	let observer = null;
-
 	const colorList = reactive([]);
 	const isLoaded = ref(false);
+	const callCnt = ref(0);
+	const skeletonCnt = computed(() => callCnt.value * 40);
 
-	useInfiniteScroll(
-	  el,
-		() => {
-			isLoaded.value = false;
-	    	getColors(3000).then(value => {colorList.push(...value);isLoaded.value = true;});
-	  },
-	)
+	useInfiniteScroll(el, () => {
+		isLoaded.value = false;
+		getColors();
+	});
 	onMounted(() => {
-		getColors(1500).then(value => {colorList.push(...value); isLoaded.value = true;});	
-		// observer = new IntersectionObserver(e => {
-		// 	if (e[0].isIntersecting) {
-		// 		isLoaded.value = false;
-		// 		getColors(3000).then(value => {colorList.push(...value);isLoaded.value = true;});
-		// 	}
-		// }, option);
-		// observer.observe(scroll.value);
-	})
+		getColors();
+	});
+
+	const getColors = () => {
+		callCnt.value += 1;
+		axios.get('/color').then(res => {
+			colorList.push(...res.data);
+			isLoaded.value = true;
+			callCnt.value -= 1;
+		});
+	};
 
 	const setScale = (event, isIn) => {
 		if (isIn) {
-			event.target.classList.add('active');	
+			event.target.classList.add('active');
 		} else {
-			event.target.classList.remove('active');	
+			event.target.classList.remove('active');
 		}
 	};
 
-	const copyColor = (color) => {
+	const copyColor = color => {
 		if (showToast.value) {
 			showToast.value = false;
-		} 
+		}
 		setTimeout(() => {
 			showToast.value = true;
-		}, );
-		
+		});
+
 		toastText.value = 'Copy it!';
 		copiedColor.value = color;
 		window.navigator.clipboard.writeText(color.backgroundColor);
 	};
-
-
-	const colorGenerator = ()=>{
-		const ARR = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
-		let hexCode = '#';
-		for(let i = 0; i < 6; i++) {
-			let rand = Math.round(Math.random() * 15);
-			hexCode += ARR[rand];
-		}
-		return hexCode;
-	};
-
-	const setFontColor = (backgroundColor) => {
-		backgroundColor = backgroundColor.slice(1,);
-		let r = parseInt(backgroundColor.slice(0, 2), 16);
-		let g = parseInt(backgroundColor.slice(2, 4), 16);
-		let b = parseInt(backgroundColor.slice(4, 6), 16);
-		return (r * 0.299 + g * 0.587 + b * 0.114) > 186 ? 'black' : 'white';
-	};
-
-	
-
-	const getColors = ms=>{
-		return new Promise(resolve=>{
-			setTimeout(()=>{
-					let colorObj = [];
-					for (let i = 0; i < 40; i++) {
-						let backgroundColor = colorGenerator();
-						colorObj.push({
-							backgroundColor,
-							fontColor: setFontColor(backgroundColor)
-						});
-				}
-					resolve(colorObj);
-				}, 
-			ms);
-		});
-	}
 </script>
 <style lang="scss" scoped>
 	.wrapper {
@@ -159,6 +131,6 @@
 		height: 100%;
 		width: 60px;
 		background: linear-gradient(to right, #f2f2f2, #dfdfdf, #f2f2f2);
-  		animation: loading 0.9s infinite linear;
+		animation: loading 0.9s infinite linear;
 	}
 </style>
